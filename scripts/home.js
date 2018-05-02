67a93dd639b878c05b89cf8d58eb9ec28311eb77
Jeannine Orejudos <jiorejudos@Jeannines-MBP.PK5001Z>
2018-05-02 15:54:37 -0400

    More Responsive Portfolio

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,13 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "nav") {
-        x.className += " responsive";
+function showMenu() {
+    var x = document.getElementById("myNav");
+    if (menuOpen === false) {
+        x.classList.add("responsive");
+        menuOpen = true;
     } else {
-        x.className = "nav";
+        x.classList.remove("responsive");
+        menuOpen = false;
     }
 }
+var navIcon = document.getElementsByClassName("icon")[0]
+var menuOpen = false;
+navIcon.addEventListener("click", showMenu)
