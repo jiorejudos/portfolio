c904d04646cd76deb1b2631d89433756c4011bb2
Jeannine Orejudos <jiorejudos@Jeannines-MBP.PK5001Z>
2018-05-16 09:59:06 -0400

    Workheader sticky

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,13 @@
-function showMenu() {
-    var x = document.getElementById("myNav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("workheader");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
